8dced424f19c1ce650d336ee5a8f1dde060c05eb
Antony <anton@drovalev.pp.ua>
2019-05-14 21:56:31 +0300

    added threading and interface

--- evaluation/stream.py
+++ evaluation/stream.py
@@ -13,7 +13,80 @@ import numpy as np
 import sys
 import os
 import dlib
+import threading
 from scipy.misc import imresize
+
+class VideoCaptureAsync:
+    def __init__(self, src=0, width=640, height=480, nframes=120):
+        self.src = src
+        self.cap = cv2.VideoCapture(self.src)
+        #self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        #self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.grabbed, self.frame = self.cap.read()
+        self.nframes = nframes
+        self.frames = []
+        self.read_lock = threading.Lock()
+
+    def set(self, var1, var2):
+        self.capset.set(var1, var2)
+
+    def start(self):
+        if self.started:
+            print('[!] Asynchroneous video capturing has already been started.')
+            return None
+        self.started = True
+        self.thread = threading.Thread(target=self.update, args=())
+        self.thread.start()
+        return self
+
+    def update(self):
+        n = 0
+        while self.started:
+            grabbed, frame = self.cap.read()
+            if grabbed:
+                with self.read_lock:
+                    self.frames.append(frame)
+                n+=1
+                if n>=self.nframes: break
+        self.started = False
+
+    def read(self,n):
+        frame = None
+        with self.read_lock:
+            if n<len(self.frames): frame = self.frames[n]
+        return frame
+
+    def stop(self):
+        self.started = False
+        self.thread.join()
+
+    def __exit__(self, exec_type, exc_value, traceback):
+        self.cap.release()
+
+
+def test(n_frames=500, width=1280, height=720, async=False):
+    detector = dlib.get_frontal_face_detector()
+    if async:
+        cap = VideoCaptureAsync(0)
+    else:
+        cap = cv2.VideoCapture(0)
+    #cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+    if async:
+        cap.start()
+    t0 = time.time()
+    i = 0
+    while i < n_frames:
+        _, frame = cap.read()
+        #dets = detector(frame, 1)
+        cv2.imshow('Frame', frame)
+        cv2.waitKey(1)
+        i += 1
+    t1 = time.time() - t0
+    print('[i] Frames per second: {:.2f} {}, async={}'.format(n_frames / t1, t1, async))
+    if async:
+        cap.stop()
+    cv2.destroyAllWindows()
 
 def square_of_mouth(points):
     det = 0
@@ -104,34 +177,39 @@ face_predictor_path = os.path.join(CURRENT_PATH,'..','common','predictors','shap
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(face_predictor_path)
 
+def empty_stream():
+    cap = VideoCaptureAsync(0)
+    m=0
+    while(True):
+        ret, frame = cap.read(m)
+        m += 1
+        cv2.putText(frame,'Press "s" and say commands',(50,100), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+frames = []
+lenf = 0
+handling = False
+mouth_frames = []
+sq = []
 
 
-
-def stream(detector,predictor):
-
-    #cap = cv2.VideoCapture('1.mpg')
-    cap = cv2.VideoCapture(0)
-    mouth_frames = []
-    sq = []
-    #split_nums = []
+def handle():
+    global handling,mouth_frames,sq
     MOUTH_WIDTH = 100
     MOUTH_HEIGHT = 50
     HORIZONTAL_PAD = 0.19
     normalize_ratio = None
     min_num = 0
     max_num = 0
-
-
     m = 0
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        
-        if(m==224):
-            break
-        # detection start
-        
+    for i in xrange(100):
+        print(i)
+        while i>=lenf: time.sleep(0.01)
+        frame = frames[i]
         dets = detector(frame, 1)
-        '''
+                
         shape = None
         for k, d in enumerate(dets):
             shape = predictor(frame, d)
@@ -147,8 +225,8 @@ def stream(detector,predictor):
                 continue
             mouth_points.append((part.x,part.y))
         np_mouth_points = np.array(mouth_points)
-        '''
-        '''
+        
+        
         sq.append(square_of_mouth(np_mouth_points))
         print(sq[m],m)
         m = m + 1
@@ -195,23 +273,145 @@ def stream(detector,predictor):
         # detection end
 
 
+    handling = False
 
-
-
-        mouth_drawed_image = frame
-        cv2.circle(mouth_drawed_image,(int(mouth_centroid[0]),int(mouth_centroid[1])),10,(0,0,0))
-        for item in np_mouth_points:
-            cv2.circle(mouth_drawed_image,(int(item[0]),int(item[1])),5,(0,255,0))
-        cv2.imshow('frame',mouth_drawed_image)
-        '''
-        cv2.imshow('frame',frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+def stream1():
+    global frames, lenf, handling
+    cap = cv2.VideoCapture(0)
+    handling = False
+    while(True):
+        ret, frame = cap.read()
+        if ret:
+            if handling:
+                if lenf<100:
+                    frames.append(frame)
+                    lenf+=1
+                    cv2.putText(frame,'Recording...',(50,100), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+                else:
+                    cv2.putText(frame,'Recognizing...',(50,100), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+                
+            else:
+                cv2.putText(frame,'Press "s" and say commands',(50,100), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+            cv2.imshow('frame',frame)
+            
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
             break
-    
+        elif key == ord('s'):
+            handling = True
+            frames = []
+            lenf = 0
+            threading.Thread(target=handle).start()
 
+
+
+def stream(detector,predictor):
+
+    #cap = cv2.VideoCapture('1.mpg')
+    cap = cv2.VideoCapture(0)
+    mouth_frames = []
+    sq = []
+    #split_nums = []
+    MOUTH_WIDTH = 100
+    MOUTH_HEIGHT = 50
+    HORIZONTAL_PAD = 0.19
+    normalize_ratio = None
+    min_num = 0
+    max_num = 0
+
+    m = 0
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        cv2.putText(frame,'Press "s" and say commands',(50,100), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+        cv2.imshow('frame',frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            while(cap.isOpened):
+                ret, frame = cap.read()
+                
+                if(m==224):
+                    break
+                # detection start
+                
+                dets = detector(frame, 1)
+                '''
+                shape = None
+                for k, d in enumerate(dets):
+                    shape = predictor(frame, d)
+                    i = -1
+                if shape is None: # Detector doesn't detect face, just return as is
+                    print('Cannot detect face')
+                    continue
+
+                mouth_points = []
+                for part in shape.parts():
+                    i += 1
+                    if i < 48: # Only take mouth region
+                        continue
+                    mouth_points.append((part.x,part.y))
+                np_mouth_points = np.array(mouth_points)
+                '''
+                '''
+                sq.append(square_of_mouth(np_mouth_points))
+                print(sq[m],m)
+                m = m + 1
+
+                if(m>20):
+                    max_num = split_dispertion(m,sq)
+                    if(max_num != -1):
+                        #split_nums.append(max_num)
+                        print(max_num)
+                        if(max_num - min_num > 50):
+                            print('COMMAND = ',max_num)
+                            video = mouth_frames[min_num:max_num]
+                            #print(video)
+                            video_data = set_data(video)
+                            #print(video_data)
+                            res = predict_videos(video_data,weight_path)
+                            print('RESULT: ' , res)
+                            min_num = max_num
+                            
+                        
+
+
+                mouth_centroid = np.mean(np_mouth_points[:, -2:], axis=0)
+
+                if normalize_ratio is None:
+                    mouth_left = np.min(np_mouth_points[:, :-1]) * (1.0 - HORIZONTAL_PAD)
+                    mouth_right = np.max(np_mouth_points[:, :-1]) * (1.0 + HORIZONTAL_PAD)
+
+                    normalize_ratio = MOUTH_WIDTH / float(mouth_right - mouth_left)
+
+                new_img_shape = (int(frame.shape[0] * normalize_ratio   ), int(frame.shape[1] * normalize_ratio))
+                resized_img = imresize(frame, new_img_shape)
+
+                mouth_centroid_norm = mouth_centroid * normalize_ratio
+
+                mouth_l = int(mouth_centroid_norm[0] - MOUTH_WIDTH / 2)
+                mouth_r = int(mouth_centroid_norm[0] + MOUTH_WIDTH / 2)
+                mouth_t = int(mouth_centroid_norm[1] - MOUTH_HEIGHT / 2)
+                mouth_b = int(mouth_centroid_norm[1] + MOUTH_HEIGHT / 2)
+
+                mouth_crop_image = resized_img[mouth_t:mouth_b, mouth_l:mouth_r]
+
+                mouth_frames.append(mouth_crop_image)
+                # detection end
+
+                mouth_drawed_image = frame
+                cv2.circle(mouth_drawed_image,(int(mouth_centroid[0]),int(mouth_centroid[1])),10,(0,0,0))
+                for item in np_mouth_points:
+                    cv2.circle(mouth_drawed_image,(int(item[0]),int(item[1])),5,(0,255,0))
+                cv2.imshow('frame',mouth_drawed_image)
+                '''
+                cv2.imshow('frame',frame)
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    return 1
+    
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    stream(detector,predictor)
+    #stream(detector,predictor)
+    stream1()
